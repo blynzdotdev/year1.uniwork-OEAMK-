3bf0e492c44b96ccc15333715fc93c434fa69c53
BlynzDotDev <113904307+blynzdotdev@users.noreply.github.com>
2022-09-19 14:27:21 +0200

    A félév feladatai

---
+++ src/lottery.cs
@@ -0,0 +1,35 @@
+using System;
+
+namespace lottomegadott_szamokkal
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int s1 = 77;  //* ez az input, nincsen console input *//
+            int s2 = 15;
+            int s3 = 44;
+            int s4 = 69;
+            int s5 = 26;
+            Console.Write("A megtett számok:{0} {1} {2} {3} {4} ", s1, s2, s3, s4, s5);
+            Console.WriteLine("\nE heti lottó számok: ");
+            Random r = new Random();
+            ///****///
+            int talalat = 0;
+            int szam;
+            for (int i = 0; i < 5; i++)
+            {
+                szam = r.Next(1, 91);
+                if (szam == s1 || szam == s2 || szam == s3 || szam == s4 || szam == s5)
+                {
+                    talalat++;
+                }
+
+
+                Console.WriteLine(" {0}", szam);
+
+            }
+            Console.Write("\n Találat: " + talalat);
+        }
+    }
+}
---
+++ src/nfactorial.cs
@@ -0,0 +1,31 @@
+using System;
+ 
+namespace nfactorial
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            static long nFactorial(int n)
+            {
+                if (n <= 1)
+                {
+                    return 1;
+                }
+                long result = 1;
+                try
+                {
+                    for (int i = 1; i <= n; i++)
+                    {
+                        result = checked(result * i);
+                    }
+                }
+                catch (OverflowException)
+                {
+                    return 0;
+                }
+                return result;
+            }
+        }
+    }
+}
